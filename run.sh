e971910a3db4b0321410f9431aa3140dae1634c6
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2026-09-08 17:18:26 +0200

    Works

---
+++ run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -x
+
+# Foo
+cd foo
+./build_foo.sh
+./build_foo/foo
+
+# Expected output:
+# 
+# open OK
+# write mo_num OK
+# write_class OK!
+# read_class OK!
+# Core            Active     
+
+rm foo.hdf5
+
+# Bar
+cd ../bar
+./build_bar.sh
+./build_bar/bar
+
+# Expected output:
+# 
+# FOO
+# open OK
+# write mo_num OK
+# write_class OK!
+# read_class OK!
+# Core            Active          
+# BAR
+# open OK
+# write mo_num OK
+# read_class OK!
+# Core            Active       
+
+rm foo.hdf5
